5b929ee25c8cd37499c2278aa8009896fda51ab6
felipe based mito <42080804+felipeyousoro@users.noreply.github.com>
2022-08-09 10:56:13 -0300

    Add files via upload

---
+++ client.py
@@ -0,0 +1,44 @@
+import socket
+import threading
+import time
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+nick = ""
+
+def receive():
+    global client 
+    
+    while True:
+        message = client.recv(1024).decode("utf-8")
+
+        if not message: break
+
+        print(message)
+
+def send():
+    global client 
+    global nick 
+
+    while True:
+        message = input()
+        client.send((nick + ": " + message).encode("utf-8"))
+
+
+if __name__ == "__main__":
+    HOST = str(input("Enter host: "))
+    PORT = int(input("Enter port: "))
+    nick = str(input("Enter your nickname: "))
+
+    client.connect((HOST, PORT))    
+
+    rcv = threading.Thread(target=receive)
+    rcv.start()
+
+    snd = threading.Thread(target=send)
+    snd.start()
+
+    while(True):
+        time.sleep(1)
+
+    client.close()
+
