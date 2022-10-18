98cdd77cea80ff96477acaadf16bc8762341a33b
felipe "based mito" yousoro <42080804+felipeyousoro@users.noreply.github.com>
2022-10-17 23:02:51 -0300

    Add files via upload

--- udp/udp falido.py
+++ udp/udp falido.py
@@ -4,6 +4,9 @@ import numpy as np
 import os
 import time 
 
+package_size = 1000
+
+
 class Peer:
     def __init__(self, ip, port):
         self.ip = ip
@@ -14,27 +17,30 @@ class Peer:
     def send_file(self, file_name, ip, port):
         file = open(file_name, "rb")
         self.send_file_header(file_name, ip, port)
-        
+        print("Header sent")    
+    
         self.socket.settimeout(0.05)
 
-        pckg_num = int(np.ceil(os.path.getsize(file_name) / 990))
+        global package_size
+        pckg_num = int(np.ceil(os.path.getsize(file_name) / (package_size - 10)))
+
         self.send_file_content(file, pckg_num, ip, port)
    
-        print("\tFile %s sent to %s:%d" % (file_name, ip, port))
+        print("File %s sent to %s:%d" % (file_name, ip, port))
         file.close()
 
     def send_file_header(self, file_name, ip, port):
         file_size = os.path.getsize(file_name)
 
         header = file_name + ";" + str(file_size)
-        header = header.zfill(1000)
+        header = header.zfill(package_size)
         header = header.encode('utf-8')
         
         ack_msg = ""
         while True:
             try:
                 self.socket.sendto(header, (ip, port))
-                ack_msg = self.socket.recv(1000)
+                ack_msg = self.socket.recv(package_size)
                 ack_msg = ack_msg.decode('utf-8').lstrip('0')
                 if ack_msg == "ACK":
                     break
@@ -42,20 +48,19 @@ class Peer:
                 continue
 
     def send_file_content(self, file, pckg_num, ip, port):
-        for i in range(1, pckg_num + 1):
-            # data = file.read(990)
-            # data = (str(i) + ";").encode('utf-8') + data
-            # data = data.zfill(1000)
+        global package_size
 
-            # self.socket.sendto(data, (ip, port))
+        for i in range(1, pckg_num + 1):
+            if i % 5000 == 0:
+                print("Sending package %d of %d" % (i, pckg_num))
             while True:
                 try:
-                    data = file.read(990)
+                    data = file.read(package_size - 10)
                     data = (str(i) + ";").encode('utf-8') + data
-                    data = data.zfill(1000)
+                    data = data.zfill(package_size)
 
                     self.socket.sendto(data, (ip, port))
-                    ack_msg = self.socket.recv(1000)
+                    ack_msg = self.socket.recv(package_size)
                     ack_msg = ack_msg.decode('utf-8').lstrip('0')
                     if ack_msg == "ACK":
                         break
@@ -68,22 +73,23 @@ class Peer:
     def receive_file(self):
         header, addr = self.receive_header()
         file_name, file_size = header.split(";")
-        
+
         self.socket.settimeout(0.1)
 
-        pckg_num = int(np.ceil(int(file_size) / 990))
+        global package_size
+        pckg_num = int(np.ceil(int(file_size) / (package_size - 10)))
         self.receive_file_content(file_name, pckg_num, addr)
 
+        print("File %s received from %s:%d" % (file_name, addr[0], addr[1]))
         # print("\tReceived file %s" % file_name)
         # print("Time spent: %s seconds" % (time.time() - start_time))
         # print("Download speed: %s bps" % ( float(file_size * 8) / (time.time() - start_time) ))
 
 
     def receive_header(self):
-
         while True:
             try:
-                header, addr = self.socket.recvfrom(1000)
+                header, addr = self.socket.recvfrom(package_size)
                 self.socket.sendto("ACK".encode('utf-8'), addr)
                 header = header.decode('utf-8').lstrip('0')
                 break
@@ -91,16 +97,19 @@ class Peer:
                 continue
 
 
-        print("\tHeader has been received")
+        print("Header has been received")
         return header, addr
 
     def receive_file_content(self, file_name, pckg_num, addr):
+        global package_size
         file = open("chegou-" + file_name, "wb")
 
         for i in range(1, pckg_num + 1):
+            if i % 5000 == 0:
+                print("Receiving package %d of %d" % (i, pckg_num))
             while True:
                 try:
-                    data, addr = self.socket.recvfrom(1000)
+                    data, addr = self.socket.recvfrom(package_size)
                     self.socket.sendto("ACK".encode('utf-8'), addr)
                     header = data[:data.find(b';')]
                     header = header.decode('utf-8').lstrip('0')
@@ -117,10 +126,10 @@ if __name__ == "__main__":
     rcv_snd = int(input("Send or receive file? (0/1): "))
 
     if(rcv_snd == 1):
-        peer = Peer("191.52.64.63", 3000)
+        peer = Peer("192.168.1.6", 3000)
         peer.socket.bind((peer.ip, peer.port))
         peer.receive_file()
     else:
-        peer = Peer("191.52.64.63", 3000)
-        peer.send_file("bg.png", "191.52.64.63", 3000)
+        peer = Peer("192.168.1.6", 3000)
+        peer.send_file("musica.flac", "192.168.1.6", 3000)
     
