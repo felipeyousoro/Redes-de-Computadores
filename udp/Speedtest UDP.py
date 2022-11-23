a65682e58f73249a023d89e24a5c474f0459a8e0
felipe "based mito" yousoro <42080804+felipeyousoro@users.noreply.github.com>
2022-11-22 22:47:44 -0300

    Add files via upload

--- udp/Speedtest UDP.py
+++ udp/Speedtest UDP.py
@@ -29,46 +29,6 @@ def choose_peer_type():
             select = 1
         sys.stdout.flush()
 
-# def choose_package_size():
-#     select = 0
-#     print("Which package size would you like to use?")
-#     while (True):
-#         if select == 0:
-#             sys.stdout.write("\r=> 1000\t   1500")
-#         elif select == 1:
-#             sys.stdout.write("\r   1000\t=> 1500")
-#         inpt = (repr(readchar.readkey()))
-#         if inpt[2] == 'r':
-#             print("")
-#             global buffer_size
-#             buffer_size = 1000 + 500 * select
-#             return select
-#         if inpt[5] == 'K':
-#             select = 0
-#         elif inpt[5] == 'M':
-#             select = 1
-
-#         sys.stdout.flush()
-
-# def choose_window_size():
-#     select = 0
-#     print("Which window size would you like to use?")
-#     while (True):
-#         if select == 0:
-#             sys.stdout.write("\r=> 2\t   4")
-#         elif select == 1:
-#             sys.stdout.write("\r   2\t=> 4")
-#         inpt = (repr(readchar.readkey()))
-#         if inpt[2] == 'r':
-#             print("")
-#             global window_size
-#             window_size = 2 + 2 * select
-#             return select
-#         if inpt[5] == 'K':
-#             select = 0
-#         elif inpt[5] == 'M':
-#             select = 1
-
 class Peer:
     def __init__(self, ip, port):
         self.ip = ip
@@ -97,25 +57,25 @@ class Peer:
     def receive_packages(self, _time):
         packages_received = 0
 
+        #use select from socket
         while True:
-            try:
+            ready = select([self.socket], [], [], 0.05)
+            if ready[0]:
                 self.socket.recvfrom(buffer_size)
                 packages_received += 1
                 break
-            except:
-                pass
+
 
         start_time = time.time()
         while time.time() - start_time < _time:
-            try:
-                self.socket.recvfrom(buffer_size)
+            ready = select([self.socket], [], [], 0.05)
+            if ready[0]:
+                data, addr = self.socket.recvfrom(buffer_size)
                 packages_received += 1
-            except:
-                pass
 
-        print("Packages sent: " + str(packages_received))
+        print("Packages received: " + str(packages_received))
         print("Packages by second: " + str(packages_received / _time))
-        print("Total bytes sent: " + str(packages_received * buffer_size * 8))
+        print("Total bytes received: " + str(packages_received * buffer_size * 8))
         print("Total bits per second: " + str(packages_received * buffer_size * 8 / _time))
         print("Total kilobits per second: " + str(packages_received * buffer_size * 8 / (_time * 1000)))
         print("Total megabits per second: " + str(packages_received * buffer_size * 8 / (_time * 1000000)))
@@ -130,9 +90,9 @@ if __name__ == "__main__":
     # choose_window_size()
 
     if(__select__ == 1):
-        peer = Peer("192.168.1.6", 3000)
+        peer = Peer("191.52.64.241", 3000)
         peer.socket.bind((peer.ip, peer.port))
         peer.receive_packages(20)
     else:
-        peer = Peer("192.168.1.6", 3000)
-        peer.send_packages("192.168.1.6", 3000, 20)
+        peer = Peer("191.52.64.241", 3000)
+        peer.send_packages("191.52.64.241", 3000, 20)
